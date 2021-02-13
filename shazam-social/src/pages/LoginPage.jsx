7dcca7262a659ba58e7896ec9afd8f9ebd745f3a
Rohan Surana <rohansurana2810@gmail.com>
2021-02-12 23:31:14 -0800

    login

--- shazam-social/src/pages/LoginPage.jsx
+++ shazam-social/src/pages/LoginPage.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
 import CenteredContentWrapper from '../modules/CenteredContentWrapper';
+import React, { Component } from "react"
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-const LoginPage = () => (
-	<CenteredContentWrapper>
-		<h1> Login </h1>
-	</CenteredContentWrapper>
-);
+firebase.initializeApp({
+	apiKey: "AIzaSyBoKinSPrKLcHlFV1AYVW5rgF_5Qjh4toM",
+	authDomain: "social-shazam.firebaseapp.com"
+  })
+
+class LoginPage  extends Component {
+	state = { isSignedIn: false }
+	uiConfig = {
+	  signInFlow: "popup",
+	  signInOptions: [
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+	  ],
+	  callbacks: {
+		signInSuccess: () => false
+	  }
+	}
+  
+	componentDidMount = () => {
+	  firebase.auth().onAuthStateChanged(user => {
+		this.setState({ isSignedIn: !!user })
+		console.log("user", user)
+	  })
+	}
+
+	render() {
+	  return (
+		  <CenteredContentWrapper>
+		<div className="App">
+		  {this.state.isSignedIn ? (
+			<span>
+			  <div>Signed In!</div>
+			  <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+			  <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+			  <h1>Welcome {firebase.auth().currentUser.email}</h1>
+			</span>
+		  ) : (
+			<StyledFirebaseAuth
+			  uiConfig={this.uiConfig}
+			  firebaseAuth={firebase.auth()}
+			/>
+		  )}
+		</div>
+		</CenteredContentWrapper>
+	  )
+	}
+  };
 
 export default LoginPage;
