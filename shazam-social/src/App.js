66f676cb4cd9ae613172166a1903b50945371952
Shivay Lamba <shivaylamba@gmail.com>
2021-02-13 10:58:38 +0530

    initial react-router

--- shazam-social/src/App.js
+++ shazam-social/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Router>
+			<main>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home </Link>
+						</li>
+						<li>
+							<Link to="/signup">Signup </Link>
+						</li>
+						<li>
+							<Link to="/login">Login </Link>
+						</li>
+					</ul>
+				</nav>
+				<Route path="/" exact component={Home} />
+				<Route path="/signup" exact component={Signup} />
+				<Route path="/login" exact component={Login} />
+			</main>
+		</Router>
+	);
 }
 
-export default App;
+const Home = () => (
+	<React.Fragment>
+		<h1> Home </h1>
+	</React.Fragment>
+);
+const Signup = () => (
+	<React.Fragment>
+		<h1> Signup </h1>
+	</React.Fragment>
+);
+const Login = () => (
+	<React.Fragment>
+		<h1> Login </h1>
+	</React.Fragment>
+);
