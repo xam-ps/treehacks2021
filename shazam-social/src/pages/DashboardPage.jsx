c298d72bd77b141806ffb9379adf2856663730bd
Max Pöschl <poeschl.max@gmail.com>
2021-02-13 13:47:02 +0100

    Add record animation to dashboard page

--- shazam-social/src/pages/DashboardPage.jsx
+++ shazam-social/src/pages/DashboardPage.jsx
@@ -1,27 +1,32 @@
 import { Button, DatePicker } from "antd";
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import CenteredContentWrapper from "../modules/CenteredContentWrapper";
 import styles from "./DashboardPage.module.scss";
-import { useHistory } from "react-router";
+import recordSvg from "../assets/img/audio.svg";
 
-const DashboardPage = () => {
-  const history = useHistory();
-  return (
-    <div className={styles.DashboardPage}>
-      <CenteredContentWrapper fullscreen={true}>
-        <h1> Discover </h1>
-        <div className={styles.recognizeMusicButton}>Recognize Music</div>
-        <Button
-          className={styles.playlistButton}
-          onClick={() => {
-            history.push("/playlist");
-          }}
-        >
-          PL
-        </Button>
-      </CenteredContentWrapper>
-    </div>
-  );
+class DashboardPage extends Component {
+  render() {
+    const {history} = this.props;
+    return (
+      <div className={styles.DashboardPage}>
+        <CenteredContentWrapper fullscreen={true}>
+          <h1> Discover </h1>
+          <div className={styles.recognizeMusicButton}>
+            <img src={recordSvg} alt="" />
+          </div>
+          <Button
+            className={styles.playlistButton}
+            onClick={() => {
+              if(history){history.push("/playlist")};
+            }}
+          >
+            PL
+          </Button>
+        </CenteredContentWrapper>
+      </div>
+    );
+  }
 };
 
 export default DashboardPage;
