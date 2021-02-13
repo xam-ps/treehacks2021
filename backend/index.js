2bd0091c5a124c4526d219b3944b6bbbe39768dc
Shivay Lamba <shivaylamba@gmail.com>
2021-02-14 03:55:47 +0530

    added ability for song recognition

---
+++ backend/index.js
@@ -0,0 +1,150 @@
+var url = require("url");
+var fs = require("fs");
+var crypto = require("crypto");
+//npm install request
+var request = require("request");
+var multer = require("multer");
+var express = require("express");
+var cors = require("cors");
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+// var upload = multer({ dest: "uploads/" });
+
+// Replace "###...###" below with your project's host, access_key and access_secret.
+var app = express();
+app.use(cors());
+
+var defaultOptions = {
+  endpoint: "/v1/identify",
+  signature_version: "1",
+  data_type: "audio",
+  secure: true,
+  access_key: "98f751a363f90b3c508d37058a37244c",
+  access_secret: "GEIyDGb0kkl1TZ3zmVKMbxl7ScYcvqi4HODUjOxw",
+  host: "identify-us-west-2.acrcloud.com",
+};
+
+function buildStringToSign(
+  method,
+  uri,
+  accessKey,
+  dataType,
+  signatureVersion,
+  timestamp
+) {
+  return [method, uri, accessKey, dataType, signatureVersion, timestamp].join(
+    "\n"
+  );
+}
+
+function sign(signString, accessSecret) {
+  return crypto
+    .createHmac("sha1", accessSecret)
+    .update(Buffer.from(signString, "utf-8"))
+    .digest()
+    .toString("base64");
+}
+
+/**
+ * Identifies a sample of bytes
+ */
+function identify(data, options, cb) {
+  var current_data = new Date();
+  var timestamp = current_data.getTime() / 1000;
+
+  var stringToSign = buildStringToSign(
+    "POST",
+    options.endpoint,
+    options.access_key,
+    options.data_type,
+    options.signature_version,
+    timestamp
+  );
+
+  var signature = sign(stringToSign, options.access_secret);
+
+  var formData = {
+    sample: data,
+    access_key: options.access_key,
+    data_type: options.data_type,
+    signature_version: options.signature_version,
+    signature: signature,
+    sample_bytes: data.length,
+    timestamp: timestamp,
+  };
+  request.post(
+    {
+      url: "http://" + options.host + options.endpoint,
+      method: "POST",
+      formData: formData,
+    },
+    cb
+  );
+}
+
+function identify_v2(data, options, cb) {
+  //npm install form-data
+  var FormData = require("form-data");
+  //npm install node-fetch
+  var fetch = require("node-fetch");
+
+  var current_data = new Date();
+  var timestamp = current_data.getTime() / 1000;
+
+  var stringToSign = buildStringToSign(
+    "POST",
+    options.endpoint,
+    options.access_key,
+    options.data_type,
+    options.signature_version,
+    timestamp
+  );
+
+  var signature = sign(stringToSign, options.access_secret);
+
+  var form = new FormData();
+  form.append("sample", data);
+  form.append("sample_bytes", data.length);
+  form.append("access_key", options.access_key);
+  form.append("data_type", options.data_type);
+  form.append("signature_version", options.signature_version);
+  form.append("signature", signature);
+  form.append("timestamp", timestamp);
+
+  fetch("http://" + options.host + options.endpoint, {
+    method: "POST",
+    body: form,
+  })
+    .then((res) => {
+      return res.text();
+    })
+    .then((res) => {
+      cb(res, null);
+    })
+    .catch((err) => {
+      cb(null, err);
+    });
+}
+
+var bitmap = fs.readFileSync("one.mp3");
+
+app.get("/", (req, res) => {
+  return res.send("Server up and running");
+});
+app.post("/search-sound", upload.single("audioFile"), (req, res) => {
+  identify(
+    Buffer.from(req.file.buffer),
+    defaultOptions,
+    function (err, httpResponse, body) {
+      if (err) {
+        console.log(err);
+        return res.json(false);
+      }
+      return res.json(JSON.parse(body));
+    }
+  );
+});
+app.listen(5000, () => {
+  console.log("Server Started");
+});
