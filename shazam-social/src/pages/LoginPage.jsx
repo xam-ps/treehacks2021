472909af0f8b1c097d51a0953291f2c34622405d
Max Pöschl <poeschl.max@gmail.com>
2021-02-13 07:35:26 +0100

    Add reset css and move pages to folder

---
+++ shazam-social/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginPage = () => (
+	<React.Fragment>
+		<h1> Login </h1>
+	</React.Fragment>
+);
+
+export default LoginPage;
---
+++ shazam-social/src/pages/SignupPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignupPage = () => (
+	<React.Fragment>
+		<h1> Signup </h1>
+	</React.Fragment>
+);
+
+export default SignupPage;
