00efb0884c75bf5126dbfceebe03520c41966a67
Shivay Lamba <shivaylamba@gmail.com>
2021-02-14 14:19:26 +0530

    privateroute

--- shazam-social/src/App.js
+++ shazam-social/src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Route, Link, Switch, BrowserRouter } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch,
+	BrowserRouter,
+} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
 import DashboardPage from "./pages/DashboardPage";
 import PlaylistPage from "./pages/PlaylistPage";
-
+import PrivateRoute from "./PrivateRoute";
 export default function App() {
 	return (
 		<Router>
 			<main>
 				<Route path="/" exact component={LandingPage} />
 				<Route path="/login" exact component={LoginPage} />
-				<Route path="/dashboard" exact component={DashboardPage} />
-				<Route path="/playlist" exact component={PlaylistPage} />
-				<Route path="/leaderboard" exact component={PlaylistPage} />
-				<Route path="/notifications" exact component={PlaylistPage} />
+				<PrivateRoute path="/dashboard" exact component={DashboardPage} />
+				<PrivateRoute path="/playlist" exact component={PlaylistPage} />
+				<PrivateRoute path="/leaderboard" exact component={PlaylistPage} />
+				<PrivateRoute path="/notifications" exact component={PlaylistPage} />
 			</main>
 		</Router>
 	);
