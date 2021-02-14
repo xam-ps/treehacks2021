2e7ff331ef157a7a3eebbb15bd12e776fd69d6fd
Max Pöschl <poeschl.max@gmail.com>
2021-02-14 02:22:36 +0100

    Integrate backend api with sound recording component

--- shazam-social/src/pages/DashboardPage.jsx
+++ shazam-social/src/pages/DashboardPage.jsx
@@ -5,7 +5,9 @@ import recordSvg from "../assets/img/audio.svg";
 import startRecordingSvg from "../assets/img/soundwave.svg";
 import Navigation from "../modules/Navigation";
 import MicRecorder from "mic-recorder-to-mp3";
+import firebase from "firebase";
 
+const baseDomain = "http://localhost:5000";
 const recorder = new MicRecorder({
   bitRate: 128,
 });
@@ -49,11 +51,11 @@ class DashboardPage extends Component {
     this.setState({ isRecording: true });
   }
 
-  stopRecording() {
+  async stopRecording() {
     recorder
       .stop()
       .getMp3()
-      .then(([buffer, blob]) => {
+      .then(async ([buffer, blob]) => {
         // do what ever you want with buffer and blob
         // Example: Create a mp3 file and play
         const file = new File(buffer, "recognize.mp3", {
@@ -61,14 +63,34 @@ class DashboardPage extends Component {
           lastModified: Date.now(),
         });
 
-        const player = new Audio(URL.createObjectURL(file));
-        player.play();
+        const formData = new FormData();
+        formData.append("audioFile", file);
+        const response = await fetch(`${baseDomain}/search-sound`, {
+          method: "POST",
+          body: formData,
+        });
+        const songData = await response.json();
+        let jsonStructure = songData.metadata.music[0];
+        var resJson = {
+          id: jsonStructure.acrid,
+          title: jsonStructure.title,
+          artist: jsonStructure.artists[0].name,
+          album: jsonStructure.album.name,
+          email: firebase.auth().currentUser.email,
+        };
+        console.log(resJson);
+        const responseStoreSong = await fetch(`${baseDomain}/songs`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: resJson,
+        });
       })
       .catch((e) => {
-        alert("We could not retrieve your message");
         console.log(e);
       });
-      this.setState({ isRecording: false });
+    this.setState({ isRecording: false });
   }
 }
 
