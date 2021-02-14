76908e351c66a634f48036d7449f4606ea161fbd
Max Pöschl <poeschl.max@gmail.com>
2021-02-14 01:06:16 +0100

    Add audio from microphone recorder and add implement version

--- shazam-social/src/pages/DashboardPage.jsx
+++ shazam-social/src/pages/DashboardPage.jsx
@@ -1,25 +1,74 @@
-import { Button, DatePicker } from "antd";
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
 import CenteredContentWrapper from "../modules/CenteredContentWrapper";
 import styles from "./DashboardPage.module.scss";
 import recordSvg from "../assets/img/audio.svg";
+import startRecordingSvg from "../assets/img/soundwave.svg";
 import Navigation from "../modules/Navigation";
+import MicRecorder from "mic-recorder-to-mp3";
+
+const recorder = new MicRecorder({
+  bitRate: 128,
+});
 
 class DashboardPage extends Component {
+  state = {
+    isRecording: false,
+  };
+
   render() {
     const { history } = this.props;
     return (
       <div className={styles.DashboardPage}>
         <CenteredContentWrapper fullscreen={true}>
           <h1> Discover </h1>
-          <div className={styles.recognizeMusicButton}>
-            <img src={recordSvg} alt="" />
+          <div
+            className={styles.recognizeMusicButton}
+            onClick={() => {
+              this.state.isRecording
+                ? this.stopRecording()
+                : this.startRecording();
+            }}
+          >
+            <img src={startRecordingSvg} alt="" />
           </div>
         </CenteredContentWrapper>
         <Navigation />
       </div>
     );
+  }
+
+  startRecording() {
+    recorder
+      .start()
+      .then(() => {
+        // something else
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+    this.setState({ isRecording: true });
+  }
+
+  stopRecording() {
+    recorder
+      .stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        // do what ever you want with buffer and blob
+        // Example: Create a mp3 file and play
+        const file = new File(buffer, "recognize.mp3", {
+          type: blob.type,
+          lastModified: Date.now(),
+        });
+
+        const player = new Audio(URL.createObjectURL(file));
+        player.play();
+      })
+      .catch((e) => {
+        alert("We could not retrieve your message");
+        console.log(e);
+      });
+      this.setState({ isRecording: false });
   }
 }
 
