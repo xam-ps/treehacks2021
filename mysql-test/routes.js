c3e6489c803a4fb3e2a10067aa455569009ab410
Shivay Lamba <shivaylamba@gmail.com>
2021-02-14 21:02:58 +0530

    test

--- mysql-test/routes.js
+++ mysql-test/routes.js
@@ -1,84 +1,91 @@
 const express = require("express");
 const router = express.Router();
-var bodyParser = require('body-parser')
-const mysql = require('mysql');
+var bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-
-
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'socialshazam',
-  });
-
-router.post("/songs",jsonParser,(req, res) => {
-
-    const id = '"' + req.body.id+ '"';
-    const title = '"' + req.body.title + '"';
-    const artist = '"'+req.body.artist + '"';
-    const album = '"' + req.body.album + '"';
-    const email = '"' + req.body.email + '"';
-
-    var addToSongs = `INSERT INTO SONGS (Id, title, artist, album) VALUES (${id},${title},${artist},${album})`;
-    connection.query(addToSongs, function (err, result) {
-        if (err) throw err;
-      });
-    
-      var addToDiscoveredSong = `INSERT INTO discoveredSongs (songId, userEmail, timestamp) VALUES (${id},${email},now())`;
-      connection.query(addToDiscoveredSong, function (err, result) {
-        if (err) throw err;
-      });
+	host: "localhost",
+	user: "root",
+	password: "password",
+	database: "socialshazam",
 });
 
-let groupId ;
-router.post("/groups",jsonParser, (req, res) => {
-    const groupName = '"' + req.body.groupName + '"';
-    const userEmail = '"' + req.body.userEmail + '"';
-    let emails = []
-    emails = req.body.emails;
-    var addToGroups = `INSERT INTO socialshazam.Groups (groupName, userEmail) VALUES (${groupName},${userEmail})`;
-    connection.query(addToGroups, function (err, result) {
-        if (err) throw err;
-      });
-      emails.push(req.body.userEmail);
-      emails.forEach(element => {
-          const ele = '"' + element + '"';
-        var addToUserInGroup = `INSERT INTO usersInGroup (groupId, userEmail) VALUES (LAST_INSERT_ID(),${ele})`;
-        console.log(addToUserInGroup);
-        connection.query(addToUserInGroup, function (err, result) {
-            if (err) throw err;
-          });
-      });
+router.post("/songs", jsonParser, (req, res) => {
+	const id = '"' + req.body.id + '"';
+	const title = '"' + req.body.title + '"';
+	const artist = '"' + req.body.artist + '"';
+	const album = '"' + req.body.album + '"';
+	const email = '"' + req.body.email + '"';
+
+	var addToSongs = `INSERT INTO SONGS (Id, title, artist, album) VALUES (${id},${title},${artist},${album})`;
+	connection.query(addToSongs, function (err, result) {
+		if (err) throw err;
+	});
+
+	var addToDiscoveredSong = `INSERT INTO discoveredSongs (songId, userEmail, timestamp) VALUES (${id},${email},now())`;
+	connection.query(addToDiscoveredSong, function (err, result) {
+		if (err) throw err;
+	});
 });
 
-router.get("/userGroupPlaylists",jsonParser, (req,res) => {
-  const userEmail = '"' + req.body.userEmail + '"';
-var getSongId =`
+let groupId;
+router.post("/groups", jsonParser, (req, res) => {
+	const groupName = '"' + req.body.groupName + '"';
+	const userEmail = '"' + req.body.userEmail + '"';
+	let emails = [];
+	emails = req.body.emails;
+	var addToGroups = `INSERT INTO socialshazam.Groups (groupName, userEmail) VALUES (${groupName},${userEmail})`;
+	connection.query(addToGroups, function (err, result) {
+		if (err) throw err;
+	});
+	emails.push(req.body.userEmail);
+	emails.forEach((element) => {
+		const ele = '"' + element + '"';
+		var addToUserInGroup = `INSERT INTO usersInGroup (groupId, userEmail) VALUES (LAST_INSERT_ID(),${ele})`;
+		console.log(addToUserInGroup);
+		connection.query(addToUserInGroup, function (err, result) {
+			if (err) throw err;
+		});
+	});
+});
+
+router.get("/userGroupPlaylists", jsonParser, (req, res) => {
+	const userEmail = '"' + req.body.userEmail + '"';
+	var getSongId = `
 select distinct(title), artist, album, socialshazam.discoveredSongs.timestamp from socialshazam.Groups 
 left join  socialshazam.usersInGroup ON socialshazam.Groups.id = socialshazam.usersInGroup.groupId and socialshazam.Groups.userEmail = ${userEmail}
 left join socialshazam.discoveredSongs on socialshazam.discoveredSongs.userEmail = socialshazam.usersInGroup.userEmail
 left join socialshazam.Songs on socialshazam.Songs.Id = socialshazam.discoveredSongs.songId
 order by socialshazam.discoveredSongs.timestamp desc;
-  `
-  connection.query(getSongId, function (err, result) {
-    result = JSON.stringify(result);
-    if (err) throw err;
-    else return res.status(200).send(result);
-  });
+  `;
+	connection.query(getSongId, function (err, result) {
+		result = JSON.stringify(result);
+		if (err) throw err;
+		else return res.status(200).send(result);
+	});
 });
 
-
-router.get("/getUserGroups",jsonParser, (req, res) => {
-    const userEmail = '"' + req.body.userEmail + '"';
-    var getUserGroups = `SELECT groupName FROM  socialshazam.Groups WHERE userEmail = ${userEmail}`;
-
-    connection.query(getUserGroups, function (err, result) {
-        if (err) throw err;
-        return res.status(200).send(result);
-      });
+router.get("/getUserSongs/:email", jsonParser, (req, res) => {
+	const userEmail = '"' + req.params.email + '"';
+	var getUserSongs = `SELECT title, artist, album FROM socialshazam.discoveredSongs
+  left join socialshazam.Songs ON socialshazam.Songs.Id  =  socialshazam.discoveredSongs.songId
+  and socialshazam.discoveredSongs.userEmail = ${userEmail} where Id IS NOT NULL
+  `;
+	connection.query(getUserSongs, function (err, result) {
+		if (err) throw err;
+		return res.status(200).send(result);
+	});
 });
 
+router.get("/getUserGroups", jsonParser, (req, res) => {
+	const userEmail = '"' + req.body.userEmail + '"';
+	var getUserGroups = `SELECT groupName FROM  socialshazam.Groups WHERE userEmail = ${userEmail}`;
+
+	connection.query(getUserGroups, function (err, result) {
+		if (err) throw err;
+		return res.status(200).send(result);
+	});
+});
 
 module.exports = router;
--- shazam-social/src/pages/PlaylistPage.jsx
+++ shazam-social/src/pages/PlaylistPage.jsx
@@ -1,73 +1,91 @@
 import { List } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CenteredContentWrapper from "../modules/CenteredContentWrapper";
 import PlaylistEntry from "../modules/PlaylistEntry";
 import styles from "./PlaylistPage.module.scss";
 import albumCoverJpg from "../assets/img/cover.jpg";
 import avatarJpg from "../assets/img/avatar.jpg";
 import Navigation from "../modules/Navigation";
+import firebase from "firebase";const baseDomain = "http://localhost:5000";
 
-const data = [
-  {
-    title: "Jungle",
-    artist: "Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: avatarJpg,
-    imgSrc: albumCoverJpg,
-  },
-  {
-    title: "Ant Design Title 2",
-    artist: "Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: avatarJpg,
-    imgSrc: albumCoverJpg,
-  },
-  {
-    title: "Ant Design Title 3",
-    artist: "Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: avatarJpg,
-    imgSrc: albumCoverJpg,
-  },
-  {
-    title:
-      "Ant Design Title 4 Ant Design Title 4 Ant Design Title 4 Ant Design Title 4",
-    artist: "Tash Sultana Tash Sultana Tash Sultana Tash Sultana Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: null,
-    imgSrc: albumCoverJpg,
-  },{
-    title: "Jungle",
-    artist: "Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: avatarJpg,
-    imgSrc: albumCoverJpg,
-  },
-  {
-    title: "Ant Design Title 2",
-    artist: "Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: avatarJpg,
-    imgSrc: albumCoverJpg,
-  },
-  {
-    title: "Ant Design Title 3",
-    artist: "Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: avatarJpg,
-    imgSrc: albumCoverJpg,
-  },
-  {
-    title:
-      "Ant Design Title 4 Ant Design Title 4 Ant Design Title 4 Ant Design Title 4",
-    artist: "Tash Sultana Tash Sultana Tash Sultana Tash Sultana Tash Sultana",
-    firstDate: "01/12/2021",
-    avatar: null,
-    imgSrc: albumCoverJpg,
-  },
-];
+
+// const data = [
+//   {
+//     title: "Jungle",
+//     artist: "Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: avatarJpg,
+//     imgSrc: albumCoverJpg,
+//   },
+//   {
+//     title: "Ant Design Title 2",
+//     artist: "Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: avatarJpg,
+//     imgSrc: albumCoverJpg,
+//   },
+//   {
+//     title: "Ant Design Title 3",
+//     artist: "Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: avatarJpg,
+//     imgSrc: albumCoverJpg,
+//   },
+//   {
+//     title:
+//       "Ant Design Title 4 Ant Design Title 4 Ant Design Title 4 Ant Design Title 4",
+//     artist: "Tash Sultana Tash Sultana Tash Sultana Tash Sultana Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: null,
+//     imgSrc: albumCoverJpg,
+//   },{
+//     title: "Jungle",
+//     artist: "Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: avatarJpg,
+//     imgSrc: albumCoverJpg,
+//   },
+//   {
+//     title: "Ant Design Title 2",
+//     artist: "Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: avatarJpg,
+//     imgSrc: albumCoverJpg,
+//   },
+//   {
+//     title: "Ant Design Title 3",
+//     artist: "Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: avatarJpg,
+//     imgSrc: albumCoverJpg,
+//   },
+//   {
+//     title:
+//       "Ant Design Title 4 Ant Design Title 4 Ant Design Title 4 Ant Design Title 4",
+//     artist: "Tash Sultana Tash Sultana Tash Sultana Tash Sultana Tash Sultana",
+//     firstDate: "01/12/2021",
+//     avatar: null,
+//     imgSrc: albumCoverJpg,
+//   },
+// ];
 
 const PlaylistPage = () => {
+  
+  const [data, setData] = useState([]);
+
+  useEffect( ()=> {
+    const baseDomain = "http://localhost:5000";
+    let email =  firebase.auth().currentUser.email;
+    const response = await fetch(`${baseDomain}/getUserSongs/${email}`, {
+      method: "GET"
+    });
+    const data = await response.json();
+    setData(data);
+  },
+  [] )
+
+  
+
   return (
     <div className={styles.PlaylistPage}>
       <CenteredContentWrapper fullscreen={true}>
